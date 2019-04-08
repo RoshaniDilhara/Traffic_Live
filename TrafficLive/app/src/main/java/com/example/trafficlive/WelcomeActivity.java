6fb9c90b8f7623b6a3f0537d7b386a31144bdda3
RoshaniDilhara <roshanidilhara7@gmail.com>
2019-04-08 14:40:59 +0530

    wel

---
+++ TrafficLive/app/src/main/java/com/example/trafficlive/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.trafficlive;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
+    }
+}
