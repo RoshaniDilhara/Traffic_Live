30f87e66e2e8922d83104c6ffe0885e839715a07
Prasad Madusanka <prasadmad96@gmail.com>
2019-04-17 12:59:24 +0530

    Added by Awuli

--- TrafficLive/app/src/main/java/com/example/trafficlive/WelcomeActivity.java
+++ TrafficLive/app/src/main/java/com/example/trafficlive/WelcomeActivity.java
@@ -17,7 +17,7 @@ public class WelcomeActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                Intent mainIntent = new Intent(WelcomeActivity.this, BtnnavActivity.class);
                 startActivity(mainIntent);
                 finish();
             }
