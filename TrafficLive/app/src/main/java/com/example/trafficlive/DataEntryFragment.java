29a205366266d686305fe44c0921421d0002d443
RoshaniDilhara <roshanidilhara7@gmail.com>
2019-04-20 09:39:01 +0530

    justTheInterface

--- TrafficLive/app/src/main/java/com/example/trafficlive/DataEntryFragment.java
+++ TrafficLive/app/src/main/java/com/example/trafficlive/DataEntryFragment.java
@@ -1,17 +1,34 @@
 package com.example.trafficlive;
 
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DataEntryFragment extends Fragment {
+
+    private static final String TAG = "DataEntryFragment";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
 
 
     public DataEntryFragment() {
@@ -22,8 +39,75 @@ public class DataEntryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_entry, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_data_entry, container, false);
+
+        Button scan_qr = (Button) view.findViewById(R.id.scan_qr);
+        scan_qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), QRActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button rec_numPlate = (Button) view.findViewById(R.id.rec_numPlate);
+        rec_numPlate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(getActivity(), NumPlateActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+        //enter date
+        mDisplayDate = (TextView) view.findViewById(R.id.date);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        getActivity(),
+                        android.R.style.Theme_DeviceDefault_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month +1;
+                Log.d(TAG,"onDataSet: dd/mm/yyy:" + year+"/"+month+"/"+dayOfMonth);
+
+                String date = year+"/"+month+"/"+dayOfMonth;
+                mDisplayDate.setText(date);
+            }
+        };
+
+        //enter time
+
+        Button timeBtn = (Button) view.findViewById(R.id.timeBtn);
+        timeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar2 = Calendar.getInstance();
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                String time = "Current Time:"+ format.format(calendar2.getTime());
+                TextView timeView = (TextView) view.findViewById(R.id.timer);
+                timeView.setText(time);
+            }
+        });
+
+
+        return view;
     }
 
 }
