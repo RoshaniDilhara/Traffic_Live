9c1863938c9f0b3b90b523d4d4600fb810f2bfc5
RoshaniDilhara <roshanidilhara7@gmail.com>
2019-05-16 21:24:55 +0530

    help fragment

--- TrafficLive/app/src/main/java/com/example/trafficlive/HelpFragment.java
+++ TrafficLive/app/src/main/java/com/example/trafficlive/HelpFragment.java
@@ -3,15 +3,26 @@ package com.example.trafficlive;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HelpFragment extends Fragment {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+
+    private SliderAdapter sliderAdapter;
 
 
     public HelpFragment() {
@@ -22,8 +33,63 @@ public class HelpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+
+        final View helpview = inflater.inflate(R.layout.fragment_help, container, false);
+
+        mSlideViewPager = helpview.findViewById(R.id.slideViewPager);
+        mDotLayout = helpview.findViewById(R.id.dotsLayout);
+
+        sliderAdapter = new SliderAdapter(getContext());
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDOtsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
+        return helpview;
     }
+
+    public void addDOtsIndicator(int position){
+
+        mDots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0;i<mDots.length;i++){
+
+            mDots[i] = new TextView(getContext());
+            mDots[i].setText(Html.fromHtml("&#8226"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.black));
+
+            mDotLayout.addView(mDots[i]);
+
+        }
+
+        if (mDots.length > 0){
+
+            mDots[position].setTextColor(getResources().getColor(R.color.gray));
+
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+
+            addDOtsIndicator(i);
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
 
 }
